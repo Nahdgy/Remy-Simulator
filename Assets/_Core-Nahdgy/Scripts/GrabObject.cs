5c76b827209674e6bb8c9b5b7c665d6540999a54
Nahdgy <113891480+Nahdgy@users.noreply.github.com>
2023-01-27 12:08:26 +0100

    Deplacement merge

--- Assets/_Core-Nahdgy/Scripts/GrabObject.cs
+++ Assets/_Core-Nahdgy/Scripts/GrabObject.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class GrabObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform _player;
+    public Transform _playerCam;
+    public float _throwForce = 10;
+  
+
+    [SerializeField] 
+    private bool _hasPlayer = false, _beingCarried = false, _touched = false;
+
+
+
+    private void Update()
     {
-        
+       float _dist = Vector2.Distance(gameObject.transform.position, _player.position); 
+
+        if(_dist <= 1.9f)
+        {
+            _hasPlayer = true;
+        }
+        else
+        {
+            _hasPlayer = false;
+        }
+
+        if(_hasPlayer == true && Input.GetButtonDown("Hold"))
+        {
+
+            GetComponent<Rigidbody>().isKinematic = true; 
+            transform.parent = _playerCam;
+            _beingCarried = true;
+        }
+
+        if(_beingCarried)
+        {
+            if(_touched) 
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                _beingCarried = false;  
+                _touched= false;
+            }
+
+            if(Input.GetButton("Shoot"))
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                _beingCarried = false;
+                GetComponent<Rigidbody>().AddForce(_playerCam.forward * _throwForce);
+            }
+
+            else if(Input.GetButtonUp("Hold"))
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                _beingCarried = false;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter()
     {
+        if(_beingCarried)
+        {
+            _touched = true;
+        }
         
     }
 }
