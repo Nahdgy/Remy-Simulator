c957e1baab5ebe897e14ab720a01db2cb1c6933f
Nahdgy <113891480+Nahdgy@users.noreply.github.com>
2023-03-24 17:46:40 +0100

    Commit 24/03

---
+++ Assets/_Core-Nahdgy/Scripts/Button.cs
@@ -0,0 +1,50 @@
+using System.Buffers.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject baseUI, difficultyUI;
+    [SerializeField]
+    private UI ui;
+    [SerializeField]
+    private float easy = 120, normal = 60, difficult = 30;
+    public void PlayButton()
+    {
+        baseUI.SetActive(false);
+        difficultyUI.SetActive(true);
+
+    }
+    public void RetourButton()
+    {
+        baseUI.SetActive(true);
+        difficultyUI.SetActive(false);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+    public void EasyButton()
+    {
+        ui.countStart = easy;
+        SceneManager.LoadScene("Nahdgy_Scene");
+    }
+    public void NormalButton()
+    {
+        ui.countStart = normal;
+        SceneManager.LoadScene("Nahdgy_Scene");
+    }
+    public void DifficultButton()
+    {
+        ui.countStart = difficult;
+        SceneManager.LoadScene("Nahdgy_Scene");
+
+    }
+    public void MenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
--- Assets/_Core-Nahdgy/Scripts/UI.cs
+++ Assets/_Core-Nahdgy/Scripts/UI.cs
@@ -1,17 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class UI : MonoBehaviour
 {
+    public float countStart, currentTime;
+    [SerializeField]
+    private Text timer;
+    private void Start()
+    {
+        currentTime = countStart;
+    }
+    private void Update()
+    {
+        CallTimer();
+    }
+
+    void CallTimer()
+    {
+        if (timer != null)
+        {
+            if (currentTime > 0)
+            {
+                currentTime -= Time.deltaTime;
+            }
+            else
+            {
+                currentTime = 0;
+            }
+            Timmer(currentTime);
+        }
+    }
+
+   
+   
     public void WinScene()
     {
         SceneManager.LoadScene("Win");
     }
 
-    public void GameOverScene()
+    private void GameOverScene()
     {
         SceneManager.LoadScene("GameOver");
+    }
+
+    private void Timmer(float timeTodisplay)
+    {
+        if(timeTodisplay < 0) 
+        {
+            timeTodisplay = 0;
+        }
+        string minutes = Mathf.FloorToInt(timeTodisplay / 60).ToString();
+        string seconds = Mathf.FloorToInt(timeTodisplay % 60).ToString("f0");
+
+        timer.text = minutes + ":" + seconds;
+
+        if(currentTime == 0)
+        {
+            GameOverScene();
+        }
     }
 }
